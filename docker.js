78ebc914d495e88116cddb2579b109680e48f1d2
swetha <swetha.ramaiah@gmail.com>
2016-02-17 13:12:29 -0800

    delete unwanted files

--- docker.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-
-router.get('/', (req, res, next) => {
-	return res.json({
-		docker: 'rocks!'
-	});
-});
-
-module.exports = router;
--- health.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-
-router.get('/', (req, res, next) => {
-	return res.json({
-		healthy: true
-	});
-});
-
-module.exports = router;
